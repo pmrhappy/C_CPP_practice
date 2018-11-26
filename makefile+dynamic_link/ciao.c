fc016f8c4fddd52df84b5e53220fdad0461e9934
Mush <pmrhappy@gmail.com>
2018-11-26 10:52:46 +0000

    add makefile and dynamic link

---
+++ makefile+dynamic_link/ciao.c
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "ciao.h"
+
+using namespace std;;
+
+
+
+void say_ciao(){
+    cout << "ciao!!\n";
+    return;
+}
+/*
+int main(){
+    cout << "This is ciao's main speaking!!\n";
+    return 0;
+}*/
---
+++ makefile+dynamic_link/hello.h
@@ -0,0 +1,6 @@
+#ifndef __HELLO_H_INCLUDED__ 
+#define __HELLO_H_INCLUDED__
+
+void say_hello();
+int* get_int_array();
+#endif
