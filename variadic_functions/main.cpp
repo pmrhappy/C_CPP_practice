a2e8a8ad13e6f831ebaa25e8e3b8057b399808b9
Mush <pmrhappy@gmail.com>
2019-01-28 16:01:28 +0800

    Add forwarding variadic parameters

--- variadic_functions/main.cpp
+++ variadic_functions/main.cpp
@@ -3,16 +3,20 @@
 
 using namespace std;
 
-void fun(const char* fmt, ...){
-    va_list args;
-    va_start(args, fmt);
+void fun(const char* fmt, va_list args){
     std::cerr << "fmt" << fmt << std::endl;
     std::cerr << "arg: " << va_arg(args, double) << std::endl;
     std::cerr << "arg: " << va_arg(args, int) << std::endl;
     std::cerr << "arg: " << static_cast<char>(va_arg(args, int)) << std::endl;
 }
 
+void forward_fun(const char* fmt, ...){
+    va_list args;
+    va_start(args, fmt);
+    fun(fmt, args);
+}
+
 int main(int, char**){
     std::cerr << "input: " << "fun(\"xxxxx\", 889, 1.25, 97)" << std::endl;
-    fun("xxxxx", 889, 1.25, 97); 
+    forward_fun("xxxxx", 889, 1.25, 97); 
 }
