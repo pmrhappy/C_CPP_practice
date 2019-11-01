6b242be868b8a70cd4bf472412f1ea0c56050eed
Mush <pmrhappy@gmail.com>
2019-11-01 18:13:33 +0800

    Add execl in fork_and_wait

--- fork_execl_and_wait/main.cpp
+++ fork_execl_and_wait/main.cpp
@@ -14,13 +14,16 @@ int main(){
         sleep(1);
         cout << "Child, end" << endl;
         sleep(1);
-        throw;
+        auto rc = execl("/bin/ls", "ls", "-alh", NULL);
+        cout << "Child, rc:" << rc << endl;
+        exit(WEXITSTATUS(rc));
     }
     else{ // Parent
         cout << "Parent, pid=" << getpid() << endl;
         int status(1);
         wait(&status); // status == 0 if the child process stop normally
-        cout << "Parent, stop waiting, child status=" << status << endl;
+        cout << "Parent, stop waiting, child status=" << WEXITSTATUS(status) << endl;
+        exit(WEXITSTATUS(status));
 
     }
 
